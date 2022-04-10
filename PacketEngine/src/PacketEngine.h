c033ba8b2880edb0e7b015e17f78f842bf2346a5
Igor <lok1gameproject.suport@gmail.com>
2022-04-10 13:34:02 +0900

    Updated premake5.lua

--- PacketEngine/src/PacketEngine.h
+++ PacketEngine/src/PacketEngine.h
@@ -4,10 +4,6 @@
 #include <stdio.h>
 #include "PacketEngine/Application.h"
 #include "PacketEngine/Log.h"
-#include "PacketEngine/Events/Event.h"
-#include "PacketEngine/Events/KeyEvent.h"
-#include "PacketEngine/Events/MouseEvent.h"
-#include "PacketEngine/Events/ApplicationEvent.h"
 
 // Entry point
 #include "PacketEngine/EntryPoint.h"
--- PacketEngine/src/PacketEngine/Events/Event.h
+++ PacketEngine/src/PacketEngine/Events/Event.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "PacketEngine/Core.h"
+
 #include <string>
 #include <functional>
 
